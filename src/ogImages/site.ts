8c962473c2b834bc38195ec9d7269c49c96d5c64
taiwei <a200206518@163.com>
2026-02-03 16:59:54 +0800

    新增logo

--- src/ogImages/site.ts
+++ src/ogImages/site.ts
@@ -1,59 +1,59 @@
-import config from '@/theme.config'
-import urlEncodedLogo from './urlEncodedLogo'
+import config from "@/theme.config";
+import urlEncodedLogo from "./urlEncodedLogo";
 
-const { title: siteTitle, description: siteDescription } = config
+const { title: siteTitle, description: siteDescription } = config;
 
 export default (accent: string, bg: string) => () => ({
-  type: 'div',
+  type: "div",
   props: {
     tw: `flex flex-col w-full h-full p-8 bg-${bg} text-${accent}`,
     children: [
       {
-        type: 'div',
+        type: "div",
         props: {
-          tw: 'flex flex-col w-full h-full justify-center items-center p-12',
+          tw: "flex flex-col w-full h-full justify-center items-center p-12",
           children: [
             {
-              type: 'div',
+              type: "div",
               props: {
                 tw: `flex flex-col`,
                 children: [
                   {
-                    type: 'div',
+                    type: "div",
                     props: {
-                      tw: 'flex',
+                      tw: "flex",
                       children: [
                         {
-                          type: 'img',
+                          type: "img",
                           props: {
                             src: urlEncodedLogo,
                             height: 128,
-                            width: 128
-                          }
+                            width: 128,
+                          },
                         },
                         {
-                          type: 'span',
+                          type: "span",
                           props: {
-                            tw: 'ml-8 text-9xl font-bold',
-                            children: [siteTitle]
-                          }
-                        }
-                      ]
-                    }
+                            tw: "ml-8 text-9xl font-bold",
+                            children: [siteTitle],
+                          },
+                        },
+                      ],
+                    },
                   },
                   {
-                    type: 'span',
+                    type: "span",
                     props: {
-                      tw: 'text-6xl mt-8 font-semibold',
-                      children: [siteDescription]
-                    }
-                  }
-                ]
-              }
-            }
-          ]
-        }
-      }
-    ]
-  }
-})
+                      tw: "text-6xl mt-8 font-semibold",
+                      children: [siteDescription],
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      },
+    ],
+  },
+});
--- src/ogImages/urlEncodedLogo.ts
+++ src/ogImages/urlEncodedLogo.ts
@@ -1,8 +1,8 @@
-import fs from 'fs'
+import fs from "fs";
 
-const LOGO_FILE = './src/assets/logo.svg'
+const LOGO_FILE = "./src/assets/logo.svg";
 
-const svgString = fs.readFileSync(LOGO_FILE, 'utf-8')
-const encodedSVG = 'data:image/svg+xml,' + encodeURIComponent(svgString)
+const svgString = fs.readFileSync(LOGO_FILE, "utf-8");
+const encodedSVG = "data:image/svg+xml," + encodeURIComponent(svgString);
 
-export default encodedSVG
+export default encodedSVG;
